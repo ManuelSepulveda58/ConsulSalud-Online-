a7c758cc1c7f970d10379afdf6cd26ea972f648e
alvaroaguilar777 <alvaro2004jose11@gmail.com>
2024-05-28 20:08:32 -0500

    Cambios/CSS detalles minimos

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -14,14 +14,14 @@ export function Contacto() {
 
       <div id="mision-vision">
         <h2>Misión</h2>
-        <p>
+        <p><strong>
           En Consusalud Online, nos comprometemos a proporcionar información confiable, actualizada y accesible sobre medicamentos y las enfermedades que pueden tratar. Nuestro objetivo es empoderar a los usuarios con el conocimiento necesario para tomar decisiones informadas sobre su salud y bienestar, promoviendo una mejor calidad de vida a través del acceso a recursos de salud verificables y comprensibles.
-        </p>
+        </strong></p>
 
         <h2>Visión</h2>
-        <p>
+        <p><strong>
           Ser la fuente de referencia líder en información sobre medicamentos y enfermedades, reconocida por nuestra precisión, integridad y dedicación a la salud de la comunidad. Aspiramos a innovar continuamente en la entrega de contenidos médicos y educativos, contribuyendo a un mundo donde cada persona tenga la información necesaria para gestionar su salud de manera efectiva y segura.
-        </p>
+        </strong></p>
       </div>
 
       <div id="contacto-info">
